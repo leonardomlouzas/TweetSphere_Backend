91a9800e2cf64d9911e8929e5140c3f17ca5ad82
leomlouzas <leonardomlouzas@gmail.com>
2023-10-16 21:46:26 -0300

    feat: Project Start

---
+++ install.sh
@@ -0,0 +1,5 @@
+python -m venv venv;
+source venv/bin/activate;
+pip install --upgrade pip;
+pip install -r requirements-dev.txt;
+pip install -e .;
---
+++ tweetsphere/app.py
@@ -0,0 +1,8 @@
+from fastapi import FastAPI
+
+
+app = FastAPI(
+    title="TweetSphere",
+    version="0.1.0",
+    description="TweetSphere is a posting app",
+)
